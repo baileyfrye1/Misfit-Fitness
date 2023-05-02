461b244e945051986984e1a504da056fd6e96a26
Bailey Frye <bailey@Baileys-MacBook-Pro.local>
2023-05-02 17:31:35 -0500

    Added basic form validation

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const form = document.querySelector('form');
-const firstName = document.querySelector('#first').value;
-const lastName = document.querySelector('#last').value;
+const firstName = document.querySelector('#first');
+const lastName = document.querySelector('#last');
 const phone = document.querySelector('#phone').value;
-const email = document.querySelector('#email').value;
-const errorNodes = document.querySelectorAll('.error');
+const email = document.querySelector('#email');
+const error = document.querySelector('.error');
 const navbar = document.querySelector('.navbar');
 const info = document.querySelector('.info-grid');
 const infoText = document.querySelectorAll('.info-text');
 const infoLabel = document.querySelectorAll('.info-label');
+const contactBtn = document.querySelector('.contact-btn');
 
 
 // Animated Navbar
@@ -21,7 +22,24 @@ for (let i = 0; i <= infoText.length - 1; i++) {
         if (e.target.tagName === 'I') {
             infoText[i].classList.toggle('show');
         }
-    })
+    });
 };
 
 // Form Validation
+
+form.addEventListener('submit', (e) => {
+    let messages = [];
+
+    if (firstName.value === '' || firstName.value == null) {
+        messages.push('First name is required')
+    } else if (lastName.value === '' || lastName.value == null) {
+        messages.push('Last name is required')
+    } else if (email.value === '' || email.value == null) {
+        messages.push('Email is required')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        error.innerText = messages.join(', ')
+    }
+});
