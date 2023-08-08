c3871442ae1624872d5f912d3651819f3d4c5e76
Bailey Frye <baileyfrye@Baileys-Mac-mini.local>
2023-08-08 16:19:57 -0500

    Added contact page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const app = () => {
     navSlide();
     infoIcon();
     formVal();
+    swiperFunc();
 };
 
 
@@ -99,37 +100,39 @@ const formVal = () => {
 }
 
 // Carousel Controls
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    rewind: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    // breakpoints: {
-    //     640: {
-    //         slidesPerView: 1,
-    //         spaceBetween: 20,
-    //     },
-    //     768: {
-    //         slidesPerView: 1,
-    //         spaceBetween: 40,
-    //     },
-    //     1024: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 50,
-    //     },
-    // },
-});
+const swiperFunc = () => {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        rewind: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        // breakpoints: {
+        //     640: {
+        //         slidesPerView: 1,
+        //         spaceBetween: 20,
+        //     },
+        //     768: {
+        //         slidesPerView: 1,
+        //         spaceBetween: 40,
+        //     },
+        //     1024: {
+        //         slidesPerView: 2,
+        //         spaceBetween: 50,
+        //     },
+        // },
+    });
+}
 
 // Execute Functions
 app();
